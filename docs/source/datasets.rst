b9f7e5972ae8cb9487e956fb58e668eedf05dc3a
jiahfong <jiahfong@gmail.com>
2020-04-23 19:22:41 +0100

    Add dataset class to handle data retrieval and load

---
+++ docs/source/datasets.rst
@@ -0,0 +1,38 @@
+.. role:: hidden
+    :class: hidden-section
+
+alr.data.datasets
+=================
+
+.. automodule:: alr.data.datasets
+.. currentmodule:: alr.data.datasets
+
+Classses
+---------
+
+
+:hidden:`DataDescription`
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autoclass:: DataDescription
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    
+
+
+:hidden:`Dataset`
+~~~~~~~~~~~~~~~~~
+
+.. autoclass:: Dataset
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    
+
+
+
+Functions
+---------
+
+
--- setup.py
+++ setup.py
@@ -68,15 +68,15 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["torch", "tqdm", "numpy", "scikit-learn"],
+    install_requires=["torch", "tqdm", "numpy", "scikit-learn", "torchvision"],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    # extras_require={
-    #     "dev": ["check-manifest"],
-    #     "test": ["coverage", "codecov", "pytest", "pytest-benchmark", "pytest-cov", "pytest-forked"],
-    # },
+    extras_require={
+        # "dev": ["check-manifest"],
+        "test": ["coverage", "codecov", "pytest", "pytest-benchmark", "pytest-cov", "pytest-forked"],
+    },
     # setup_requires=["pytest-runner"],
 )
