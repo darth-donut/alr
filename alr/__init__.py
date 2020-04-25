b58f0c092154ed305f90e3e8f2c99f853505a80c
jiahfong <jiahfong@gmail.com>
2020-04-25 18:35:44 +0100

    Version 0.0.0b5

--- alr/__init__.py
+++ alr/__init__.py
@@ -19,7 +19,7 @@ from alr.acquisition import AcquisitionFunction
 from alr.modules.dropout import replace_dropout
 from alr.utils import _DeviceType, range_progress_bar, progress_bar
 
-__version__ = '0.0.0b4'
+__version__ = '0.0.0b5'
 
 
 @dataclass
