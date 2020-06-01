42d04556275cbcea5bf3ae67f76ad0449e34d9c2
jiahfong <jiahfong@gmail.com>
2020-06-01 15:03:10 +0100

    Update diagostics plotter

--- alr/training/diagnostics/__init__.py
+++ alr/training/diagnostics/__init__.py
@@ -85,11 +85,12 @@ def ece_plot(ece_E, axis, cmap: Optional[str] = 'viridis'):
     axis.set_ylabel("ECE")
     axis.set_xlabel("Epoch")
     axis.set_xticks(range(1, E + 1))
+    axis.set_xticklabels(range(1, E + 1), rotation=45)
 
 
 def plot_entropy(ent_E_N, num_classes, axis, cmap: Optional[str] = 'viridis'):
     cmap = cm.get_cmap(cmap)
-    bplot = axis.boxplot(ent_E_N.T, patch_artist=True)
+    bplot = axis.boxplot(ent_E_N.T, patch_artist=True, showfliers=False)
     E = ent_E_N.shape[0]
     max_ent = num_classes * ((-1 / num_classes) * np.log(1 / num_classes))
     for e, patch in enumerate(bplot['boxes']):
@@ -99,22 +100,64 @@ def plot_entropy(ent_E_N, num_classes, axis, cmap: Optional[str] = 'viridis'):
     axis.set_ylim(bottom=-0.05, top=max_ent)
     axis.set_yticks(np.linspace(0, max_ent, 5))
     axis.set_title("Entropy")
+    axis.set_xticklabels(range(1, E + 1), rotation=45)
 
 
 # todo(harry): can accommodate iterations too
-def plot_accuracy(acc_E, axis, cmap: Optional[str] = 'viridis'):
+def plot_accuracy(pool_acc_E, val_acc_E, axis, cmap: Optional[str] = 'viridis'):
     cmap = cm.get_cmap(cmap)
-    E = acc_E.shape[0]
-    axis.plot(range(1, E + 1), acc_E)
+    E = pool_acc_E.shape[0]
+    assert val_acc_E.shape[0] == E
+    axis.plot(range(1, E + 1), pool_acc_E, label="pool")
+    axis.plot(range(1, E + 1), val_acc_E, label="val")
     axis.set_title("Accuracy")
     axis.set_xlabel("Epoch")
     axis.set_ylabel("Accuracy")
     axis.set_xticks(range(1, E + 1))
+    axis.set_xticklabels(range(1, E + 1), rotation=45)
+    axis.legend()
 
 
-def diagnostics(root: str):
-    root = Path(root)
-    pkls = list(root.rglob("*.pkl"))
+def plot_sample_size(metric: dict, axis):
+    y = metric['history']['train_size']
+    x = len(y)
+    axis.plot(range(1, x + 1), y)
+    axis.set_xticks(range(1, x + 1))
+    axis.set_title("Training set size")
+    axis.set_xlabel("Epoch")
+    axis.set_ylabel("Training set size")
+    axis.set_xticklabels(range(1, x + 1), rotation=45)
+
+
+def plot_val_loss(metric: dict, axis):
+    y = metric['history']['val_loss']
+    x = len(y)
+    axis.plot(range(1, x + 1), y)
+    axis.set_xticks(range(1, x + 1))
+    axis.set_title("Validation Loss")
+    axis.set_xlabel("Epoch")
+    axis.set_ylabel("Loss")
+    axis.set_xticklabels(range(1, x + 1), rotation=45)
+
+
+def get_val_acc(metric: dict):
+    return np.array(metric['history']['val_acc'])
+
+
+def plot_labelled_classes(metric: dict, axis):
+    x, y = np.unique(metric['labelled_classes'], return_counts=True)
+    axis.bar(x, y)
+    axis.set_xlabel("Class")
+    axis.set_ylabel("Counts")
+    axis.set_title("BALD-acquired classes (so far)")
+
+
+def diagnostics(calib_metrics: str, metrics: str):
+    calib_metrics = Path(calib_metrics)
+    metrics = Path(metrics)
+    pkls = list(calib_metrics.rglob("*.pkl"))
+    with open(metrics, "rb") as fp:
+        metrics = pickle.load(fp)
 
     def num_sort(fname: Path):
         basename = fname.name
@@ -154,13 +197,19 @@ def diagnostics(root: str):
     ece_E = np.stack(ece, axis=0)
     entropy_E_N = np.stack(entropy, axis=0)
 
-    fig, axes = plt.subplots(3, 2, figsize=(2 * 5, 3 * 5))
+    fig, axes = plt.subplots(3, 3, figsize=(3 * 5, 3 * 5))
     axes = axes.flatten()
     confidence_plot(confidences_E_N, proportions_E_N, axes[0])
-    reliability_hist_plot(bins_E_M, counts_E_N, axes[2])
-    reliability_plot(bins_E_M, bin_accuracy_E_N, counts_E_N, axes[4])
     ece_plot(ece_E, axes[1])
-    plot_entropy(entropy_E_N, num_classes=10, axis=axes[3])
-    plot_accuracy(accuracies_E, axis=axes[5])
-    for ax in axes:
-        ax.grid()
+    plot_val_loss(metrics, axes[2])
+    reliability_hist_plot(bins_E_M, counts_E_N, axes[3])
+    plot_entropy(entropy_E_N, num_classes=10, axis=axes[4])
+    plot_labelled_classes(metrics, axis=axes[5])
+    reliability_plot(bins_E_M, bin_accuracy_E_N, counts_E_N, axes[6])
+    plot_accuracy(accuracies_E, get_val_acc(metrics), axis=axes[7])
+    plot_sample_size(metrics, axes[8])
+    plt.suptitle(f"Pool size = {entropy_E_N.shape[-1]:,}", y=1.)
+    for i, ax in enumerate(axes):
+        if i % 3 == 0:
+            ax.grid()
+    fig.tight_layout()
